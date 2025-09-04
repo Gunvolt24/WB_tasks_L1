008ba762ac138e1541142a9eed0ae86e50679f3d
Andrey Voronin <voronin241996@yandex.ru>
2025-09-04 18:25:40 +0500

    fix: :recycle: Fixed 1.11 task

--- L1.11/main.go
+++ L1.11/main.go
@@ -2,15 +2,23 @@ package main
 
 import "fmt"
 
-// Функция для нахождения пересечения двух срезов при помощи вложенного цикла
-func intersection(a, b []int) []int {
-	var result []int
-	for _, elem1 := range a {
-		for _, elem2 := range b {
-			if elem1 == elem2 {
-				result = append(result, elem1)
-				break
-			}
+// Дженерик-функция для нахождения пересечения двух множеств, если T - сравнимый тип.
+func intersection[T comparable](a, b []T) []T {
+	// Создаем мапу для хранения элементов первого среза
+	m := make(map[T]struct{}, len(a))
+
+	// Заполняем мапу элементами из первого среза
+	for _, v := range a {
+		m[v] = struct{}{}
+	}
+
+	// Результирующий срез для пересечения
+	result := make([]T, 0)
+	// Проверяем элементы второго среза на наличие в мапе
+	for _, v := range b {
+		if _, found := m[v]; found {
+			result = append(result, v) // Добавляем пересекающийся элемент в результат
+			delete(m, v)               // Удаляем элемент из мапы, чтобы избежать дубликатов
 		}
 	}
 	return result
